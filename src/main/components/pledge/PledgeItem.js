2d49c88d04eba1f8e53c881fb823560cc2ae1d77
Richard Seaman <rdgseaman@gmail.com>
2019-06-10 22:43:51 +0100

    added reward animations

--- src/main/components/pledge/PledgeItem.js
+++ src/main/components/pledge/PledgeItem.js
@@ -8,6 +8,7 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
 import Button from "@material-ui/core/Button";
+import Reward from "react-rewards";
 
 import CostLevel from "./CostLevel";
 
@@ -18,52 +19,73 @@ const styles = theme => ({
   }
 });
 
-const PledgeItem = props => {
-  const {
-    classes,
-    pledge,
-    onAddCommitment,
-    onDeleteCommitment,
-    commitment
-  } = props;
-  return (
-    <Card className={classes.card}>
-      <CardHeader
-        title={pledge.title}
-        subheader={`${pledge.counter || 0} commitments`}
-      />
-      <CardContent>
-        {pledge.desc && (
-          <Typography variant="body2" color="textSecondary" component="p">
-            {pledge.desc}
-          </Typography>
-        )}
-        <CostLevel level={pledge.cost} />
-      </CardContent>
-      <CardActions>
-        <Button
-          className={classes.firstButton}
-          size="small"
-          color="primary"
-          onClick={() => alert("Not ready yet...")}
-        >
-          Learn More
-        </Button>
-        <Button
-          size="small"
-          color={commitment ? "secondary" : "primary"}
-          onClick={
-            commitment
-              ? () => onDeleteCommitment(commitment)
-              : () => onAddCommitment(pledge.id)
-          }
-        >
-          {commitment ? "Revoke Commitment" : "Make Commitment"}
-        </Button>
-      </CardActions>
-    </Card>
-  );
-};
+class PledgeItem extends React.Component {
+  render() {
+    const {
+      classes,
+      pledge,
+      onAddCommitment,
+      onDeleteCommitment,
+      commitment
+    } = this.props;
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          title={pledge.title}
+          subheader={`${pledge.counter || 0} commitments`}
+        />
+        <CardContent>
+          {pledge.desc && (
+            <Typography variant="body2" color="textSecondary" component="p">
+              {pledge.desc}
+            </Typography>
+          )}
+          <CostLevel level={pledge.cost} />
+        </CardContent>
+        <CardActions>
+          <Button
+            className={classes.firstButton}
+            size="small"
+            color="primary"
+            onClick={() => alert("Not ready yet...")}
+          >
+            Learn More
+          </Button>
+          <Reward
+            ref={ref => {
+              this.reward = ref;
+            }}
+            type={commitment ? "emoji" : "confetti"}
+            config={{
+              emoji: commitment
+                ? ["😡", "🔥", "❓", "❗"]
+                : ["♻️", "🌈", "☀️", "🌱"],
+              elementSize: commitment ? 30 : 15
+            }}
+          >
+            <Button
+              size="small"
+              color={commitment ? "secondary" : "primary"}
+              onClick={
+                commitment
+                  ? () => {
+                      this.reward.rewardMe();
+                      onDeleteCommitment(commitment);
+                    }
+                  : () => {
+                      this.reward.rewardMe();
+                      onAddCommitment(pledge.id);
+                    }
+              }
+            >
+              {commitment ? "Revoke Commitment" : "Make Commitment"}
+            </Button>
+          </Reward>
+        </CardActions>
+      </Card>
+    );
+  }
+}
 
 PledgeItem.propTypes = {
   classes: PropTypes.object.isRequired,
